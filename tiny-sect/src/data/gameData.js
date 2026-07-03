efec6cfd321a80ca788cbf9bfcc90631f08563a1
VoViHrO <vovihro@gmail.com>
2026-07-03 20:38:37 +0300

    Add files via upload

---
+++ tiny-sect/src/data/gameData.js
@@ -0,0 +1,16 @@
+export const gameData = {
+    buildings: {
+        hall: { name: 'Sect Hall', maxLevel: 4 },
+        alchemy: { name: 'Alchemy Lab', maxLevel: 4 },
+        talismans: { name: 'Talisman Workshop', maxLevel: 4 },
+        forge: { name: 'Spirit Forge', maxLevel: 4 },
+        warehouse: { name: 'Warehouse', maxLevel: 4 }
+    },
+    zones: {
+        meadow: { name: 'Meadow', maxLevel: 4 },
+        forest: { name: 'Forest', maxLevel: 4 },
+        ruins: { name: 'Ancient Ruins', maxLevel: 4 },
+        lake: { name: 'Spirit Lake', maxLevel: 4 }
+    },
+    cultivatorRanks: ['Mortal', 'Qi Condensation', 'Foundation', 'Core', 'Nascent Soul']
+};
